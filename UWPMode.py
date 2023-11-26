134ed55c82946e33bc3335e174ef8fba2bfa105f
Justin Lin <61717681+justinlin099@users.noreply.github.com>
2023-11-26 23:00:15 +0800

    Update UWPMode

--- UWPMode.py
+++ UWPMode.py
@@ -6,5 +6,17 @@ import os
 import json
 import subprocess
 import UWPScanner
+import tkinter as tk
+from tkinter import filedialog
+import ttkbootstrap as ttk
 
 UWPList = UWPScanner.ScanUWP()
+with open("refDB.json", "r") as refFile:
+    refList=json.load(refFile)
+    
+#比較 UWP 應用程式清單，找出refDB.json中沒有的應用程式
+for uwpApp in UWPList["UWPApps"]:
+    if uwpApp not in refList["UWPApps"]:
+        print("新增應用程式：",uwpApp)
+        
+#選取要寫入DB的路徑
